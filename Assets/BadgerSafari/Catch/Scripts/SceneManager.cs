c2bd8a8b109bad10ac103238eebfec48e48ead15
GeoffreyXue <xuegeoff@gmail.com>
2024-03-12 13:51:15 -0500

    add simple random spawn of badger in catch scene

--- Assets/BadgerSafari/Catch/Scripts/SceneManager.cs
+++ Assets/BadgerSafari/Catch/Scripts/SceneManager.cs
@@ -1,13 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject badgerPrefab;
+
+    private float spawnXRange = 10;
+    private float spawnZRange = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 randomPosition = new Vector3(Random.Range(-spawnXRange, spawnXRange), 0, Random.Range(-spawnZRange, spawnZRange));
+        Instantiate(badgerPrefab, randomPosition, Quaternion.identity);
     }
 
     // Update is called once per frame
