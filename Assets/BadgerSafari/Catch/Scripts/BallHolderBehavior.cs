fe7d33b2ea77a357ef018eb576ed7e48ffac5878
GeoffreyXue <xuegeoff@gmail.com>
2024-04-18 19:09:41 -0500

    Use code to spawn catch balls, fix catch ball collisions

--- Assets/BadgerSafari/Catch/Scripts/BallHolderBehavior.cs
+++ Assets/BadgerSafari/Catch/Scripts/BallHolderBehavior.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 /// <summary>
 /// Handles ball holder behavior.
 /// </summary>
-public class BallHolderBehavior : MonoBehaviour {}
+public class BallHolderBehavior : MonoBehaviour {
+    [SerializeField]
+
+    private GameObject catchBallPrefab;
+
+    // on awake, grab all socket interactables
+    void Awake() {
+        XRSocketInteractor[] socketInteractors = GetComponentsInChildren<XRSocketInteractor>();
+        // instantiate and add a catch ball prefab to each socket interactor
+        foreach (XRSocketInteractor socketInteractor in socketInteractors) {
+            GameObject catchBall = Instantiate(catchBallPrefab, socketInteractor.transform.position, Quaternion.identity);
+            catchBall.transform.localPosition = Vector3.zero;
+            catchBall.transform.localRotation = Quaternion.identity;
+            socketInteractor.startingSelectedInteractable = catchBall.GetComponent<XRBaseInteractable>();
+        }
+    }
+}
